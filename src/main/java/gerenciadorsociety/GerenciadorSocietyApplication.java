288f4ed3024c8fa72d75506b89f4300ecd313a9d
VitorVieira <vitorhvvieira@gmail.com>
2023-12-11 20:16:15 -0300

    Commit inicial

---
+++ src/main/java/gerenciadorsociety/GerenciadorSocietyApplication.java
@@ -0,0 +1,13 @@
+package gerenciadorsociety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorSocietyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorSocietyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gerenciadorsociety/GerenciadorSocietyApplicationTests.java
@@ -0,0 +1,13 @@
+package gerenciadorsociety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorSocietyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
